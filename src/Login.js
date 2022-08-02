5339b925c14348e3acaf5c54fd80d27c43dc9805
Anandu-Raveendran <47anandu007@gmail.com>
2022-08-02 15:21:30 -0400

    login page with bugs

--- src/Login.js
+++ src/Login.js
@@ -1,33 +1,74 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class LoginPage extends Component {
     constructor(props) {
         super(props);
     }
-    state = {  }
-    render() { 
-        return (  
+    state = {
+        loginStatus: false,
+        email: "",
+        password: "",
+    }
+
+    loginNow() {
+        const options = {
+            method: 'POST',
+            url: 'http://localhost:3001/v1/users/loginUser',
+            data: {
+                email: this.state.email,
+                password: this.state.password
+            },
+        };
+
+        // send the request
+        axios(options)
+            .then(res => {
+                this.setState({ loginStatus: true });
+                console.log("Login Successful")
+            })
+            .catch((reason) => {
+                if (reason.response.status === 400) {
+                    // Handle 400
+                } else {
+                    // Handle else
+                }
+                console.log(reason.message)
+            })
+    }
+
+    handleEmailChange(param) {
+        this.setState({ email: param.target.value });
+        console.log(param.target.value)
+    }
+
+    handlePasswordChange(param) {
+        this.setState({ password: param.target.value });
+    }
+
+    render() {
+        return (
             <section className="loginSection">
                 <form className="loginForm">
-                <fieldset className="fieldset">
-                <legend>Login</legend>
-                <ul>
-                    <li>
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" required/>
-                    </li>
-                    <li>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" required/>
-                    </li>
-                </ul>
-                </fieldset>
-                <button>Submit</button>
-                <button type="button">Create Account</button>
-            </form>
-          </section>
+                    <fieldset className="fieldset">
+                        <legend>Login</legend>
+                        <ul>
+                            <li>
+                                <label for="username">Username:</label>
+                                <input type="text" id="username" required onChange={this.handleEmailChange} />
+                            </li>
+                            <li>
+                                <label for="password">Password:</label>
+                                <input type="password" id="password" required onChange={this.handlePasswordChange} />
+                            </li>
+                        </ul>
+                    </fieldset>
+                    <button onClick={this.loginNow()}>Submit</button>
+                    <button >Create Account</button>
+                </form>
+            </section>
         );
     }
 }
- 
+
 export default LoginPage;
--- src/ProjectList.js
+++ src/ProjectList.js
@@ -10,42 +10,50 @@ class ProjectList extends Component {
     constructor(props) {
         super(props);
     }
-    state = { 
+    state = {
         persons: []
-     }
+    }
 
 
-  componentDidMount() {
-    axios.get(`http://localhost:3001/v1/task/getAllTasks `)
-    .then(res => {
-        const persons = res.data.results;
-        this.setState({ persons });
-      })
-  }
-   
-    render() { 
-      
-        return (  
+    componentDidMount() {
+        axios.get(`http://localhost:3001/v1/task/getAllTasks `)
+            .then(res => {
+                const persons = res.data.results;
+                this.setState({ persons });
+            })
+            .catch((reason) => {
+                if (reason.response.status === 400) {
+                    // Handle 400
+                } else {
+                    // Handle else
+                }
+                console.log(reason.message)
+            })
+    }
+
+    render() {
+
+        return (
             <div>
-            <h1>Hello ! Projects Name</h1>
-            {/* <LoginPage/> */}
-           
-            <ul>
-            {
-          this.state.persons
-            .map(person =>
-                <li key={person.id}>
-                <TaskList name={person.name} task={person}/>
-                </li>
-            )
-        }
-            </ul> 
+                <h1>Hello ! Projects Name</h1>
+                {/* <LoginPage/> */}
+
+                <ul>
+                    {
+                        this.state.persons
+                            .map(person =>
+                                <li key={person.id}>
+                                    <TaskList name={person.name} task={person} />
+                                </li>
+                            )
+                    }
+                </ul>
             </div>
-            
+
         );
     }
 }
- 
+
 export default ProjectList;
 
 
