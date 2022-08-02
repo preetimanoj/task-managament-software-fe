61e4381c95aa5983e2d40bcfd510091cceaf1aee
Anandu-Raveendran <47anandu007@gmail.com>
2022-08-02 16:45:34 -0400

    login signup

--- src/Login.js
+++ src/Login.js
@@ -1,81 +1,79 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
+let navigate = useNavigate();
 
+function loginNow() {
+    const options = {
+        method: 'POST',
+        url: 'http://localhost:3001/v1/users/loginUser',
+        data: {
+            email: this.state.email,
+            password: this.state.password
+        },
+    };
 
-function Login() {
-    let navigate = useNavigate();
-    navigate('/user');
-    console.log("logged in");
+    // send the request
+    axios(options)
+        .then(res => {
+            this.setState({ loginStatus: true });
+            console.log("Login Successful")
+
+            if (res.data.role == "admin") {
+                navigate('/admin');
+            } else {
+                navigate('/user');
+            }
+        })
+        .catch((reason) => {
+            if (reason.response.status === 400) {
+                // Handle 400
+            } else {
+                // Handle else
+            }
+            console.log(reason.message)
+        })
 }
-class LoginPage extends Component {
-    constructor(props) {
-        super(props);
-    }
-    state = {
-        loginStatus: false,
-        email: "",
-        password: "",
-    }
 
-    loginNow() {
-        const options = {
-            method: 'POST',
-            url: 'http://localhost:3001/v1/users/loginUser',
-            data: {
-                email: this.state.email,
-                password: this.state.password
-            },
-        };
 
-        // send the request
-        axios(options)
-            .then(res => {
-                this.setState({ loginStatus: true });
-                console.log("Login Successful")
-            })
-            .catch((reason) => {
-                if (reason.response.status === 400) {
-                    // Handle 400
-                } else {
-                    // Handle else
-                }
-                console.log(reason.message)
-            })
-    }
 
-    handleEmailChange(param) {
+function LoginPage() {
+    const [loginStatus, setLoginStatus] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    function handleEmailChange(param) {
         this.setState({ email: param.target.value });
         console.log(param.target.value)
     }
 
-    handlePasswordChange(param) {
+    function handlePasswordChange(param) {
         this.setState({ password: param.target.value });
     }
 
-    render() {
-        return (
-            <section className="loginSection">
-                <form className="loginForm">
-                    <fieldset className="fieldset">
-                        <legend>Login</legend>
-                        <ul>
-                            <li>
-                                <label for="username">Username:</label>
-                                <input type="text" id="username" required onChange={this.handleEmailChange} />
-                            </li>
-                            <li>
-                                <label for="password">Password:</label>
-                                <input type="password" id="password" required onChange={this.handlePasswordChange} />
-                            </li>
-                        </ul>
-                    </fieldset>
-                    <button onClick={this.loginNow()}>Submit</button>
-                    <button >Create Account</button>
-                </form>
-            </section>
-        );
-    }
+
+    return (
+        <section className="loginSection" >
+            <form className="loginForm">
+                <fieldset className="fieldset">
+                    <legend>Login</legend>
+                    <ul>
+                        <li>
+                            <label for="username">Username:</label>
+                            <input type="text" id="username" required onChange={handleEmailChange} />
+                        </li>
+                        <li>
+                            <label for="password">Password:</label>
+                            <input type="password" id="password" required onChange={handlePasswordChange} />
+                        </li>
+                    </ul>
+                </fieldset>
+                <button onClick={loginNow()}>Login</button>
+                <button >Create Account</button>
+            </form>
+        </section>
+    );
 }
 
 export default LoginPage;
--- src/Signup.js
+++ src/Signup.js
@@ -4,34 +4,74 @@ class SignUpPage extends Component {
     constructor(props) {
         super(props);
     }
-    state = {  }
-    render() { 
-        return (  
+    state = {
+        loginStatus: false,
+        email: "",
+        password: "",
+    }
+
+    signupNow() {
+        const options = {
+            method: 'POST',
+            url: 'http://localhost:3001/v1/users/loginUser',
+            data: {
+                email: this.state.email,
+                password: this.state.password
+            },
+        };
+
+        // send the request
+        axios(options)
+            .then(res => {
+                this.setState({ loginStatus: true });
+                console.log("Login Successful")
+            })
+            .catch((reason) => {
+                if (reason.response.status === 400) {
+                    // Handle 400
+                } else {
+                    // Handle else
+                }
+                console.log(reason.message)
+            })
+    }
+
+    handleEmailChange(param) {
+        this.setState({ email: param.target.value });
+        console.log(param.target.value)
+    }
+
+    handlePasswordChange(param) {
+        this.setState({ password: param.target.value });
+    }
+
+    render() {
+        return (
             <section className="loginSection">
                 <form className="loginForm">
-                <fieldset className="fieldset">
-                <legend>Sign Up!</legend>
-                <ul>
-                    <li>
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" required/>
-                    </li>
-                    <li>
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" required/>
-                    </li>
-                    <li>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" required/>
-                    </li>
-                </ul>
-                </fieldset>
-                <button>Submit</button>
-                <button type="button">Have an Account?</button>
-            </form>
-          </section>
+                    <fieldset className="fieldset">
+                        <legend>Sign Up!</legend>
+                        <ul>
+                            <li>
+                                <label for="username">Username:</label>
+                                <input type="text" id="username" required />
+                            </li>
+                            <li>
+                                <label for="email">Email:</label>
+                                <input type="email" id="email" required onChange={this.handleEmailChange} />
+                            </li>
+                            <li>
+                                <label for="password">Password:</label>
+                                <input type="password" id="password" required onChange={this.handlePasswordChange} />
+                            </li>
+                        </ul>
+                    </fieldset>
+                    <button onClick={this.signupNow()}>Submit</button>
+                    <button type="button">Have an Account?</button>
+                </form>
+            </section>
         );
     }
 }
- 
+
 export default SignUpPage;
